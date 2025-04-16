67ad2410a6cd6f95366ffa3e498d7ecf603e6da7
Paul Osborne <paul.osborne@fastly.com>
2025-04-15 22:42:07 -0500

    Add support for nightly/periodic CI runs
    Work in Progress, gather some CI results first.  On aarch64
    macos at least, gettings some cycle counts that are all over
    the place.

---
+++ benchmarks/sightglass-to-continuous.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+#
+# Helper script that transforms JSON benchmark results
+# from sightglass into the format that can be used by
+# the https://github.com/marketplace/actions/continuous-benchmark
+# Github action, custom JSON.
+
+import argparse
+from dataclasses import dataclass
+import json
+import os
+import pathlib
+
+# Sightglass Data format:
+# [
+#   {
+#     "arch": "...",
+#     "engine": "...",
+#     "wasm": "path/to/benchmark.wasm",
+#     "process": "...",
+#     "iteration": 0,
+#     "phase": "...",
+#     "event": "nanoseconds/cycles/...",
+#     "count: "..."
+#   },
+#   ...
+# ]
+#
+# Continuous Benchmark Custom (smaller is better) JSON Format:
+# [
+#   {
+#     "name": "...",
+#     "unit": "...",
+#     "value": "...",
+#     // optionally, range/extra for tooltips
+#   }
+# ]
+#
+# With the transform, we don't aim for a full translation of data.
+# Instead, we create data points for compilation cycles with the
+# value being the mean cycle time.  In the extras (tooltip), we
+# provide other metadata including:
+# - Engine Git Hash
+# - Max
+# - Min
+# - Time measure
+
+@dataclass
+class ReducedSample:
+    name: str
+    arch: str
+    event: str
+    engine: str
+    count: int
+
+    def series_name(self):
+        return f"{self.arch} - {self.name}"
+
+def wasm_path_to_benchmark_name(wasm_path: str) -> str:
+    splits = wasm_path.split("/")
+    if splits[-1] == "benchmark.wasm":
+        # E.g. noop/benchmark.wasm -> noop
+        return splits[-2]
+    else:
+        # E.g. libsodium/libsodium-box7.wasm -> libsodium-box7
+        return splits[-1].replace(".wasm", "")
+
+
+def transform(sightglass_data: [dict], measure: str) -> [dict]:
+    series_map = {}
+    for sample in sightglass_data:
+        phase = sample["phase"]
+        if phase != "Execution":
+            continue
+
+        s = ReducedSample(
+            arch=sample["arch"],
+            engine=sample["engine"],
+            name=wasm_path_to_benchmark_name(sample["wasm"]),
+            event=sample["event"],
+            count=int(sample["count"])
+        )
+        series_map.setdefault(s.series_name(), []).append(s)
+
+    output = []
+    for series_name, samples in series_map.items():
+        matching_samples = [s.count for s in samples if s.event == measure]
+
+        # yes, this could be done more efficiently
+        count = len(matching_samples)
+        min_ = min(matching_samples)
+        max_ = max(matching_samples)
+        sum_ = sum(matching_samples)
+        avg = sum_ / count
+
+        # there might be a better stastical measure here as this doesn't
+        # factor in confidence levels or anything, but is intended to give
+        # a picture of whether there are any significant outlier samples.
+        max_variance_from_mean = max(max_ - avg, avg - min_)
+        max_variance_as_pct = (max_variance_from_mean / avg) * 100
+
+        output.append({
+            "name": series_name,
+            "unit": measure,
+            "value": avg,
+            "extra": f"Max: {max_}, Min: {min_}, ±{max_variance_as_pct:.2f}%"
+        })
+
+    return output
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out", "-o", default="out.json",
+                        type=pathlib.Path, help="Custom JSON output path")
+    parser.add_argument("--measure", "-m", default="cycles",
+                        help="The measure to map as the value for the output stream (cycles, perf-counter, etc.)")
+    parser.add_argument("json_in", nargs="+",
+                        type=pathlib.Path, help="Sightglass JSON input file(s)")
+    args = parser.parse_args()
+    input_samples = []
+    for path in args.json_in:
+        with open(path) as json_f:
+            input_samples.extend(json.load(json_f))
+
+    transformed = transform(input_samples, args.measure)
+    with open(args.out, "w") as json_out:
+        json.dump(transformed, json_out)
+
+if __name__ == '__main__':
+    main()
